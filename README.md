4035f1bfce59bf9c8dbf3f584eaa32c27cfad38a
kgryte <kgryte@gmail.com>
2015-05-07 22:51:40 -0700

    [UPDATE] dotfiles. Makefile. README. copy and accessor options. allow comparison btwn object arrays. [TESTS] arg validation. code pathways.

--- README.md
+++ README.md
@@ -16,17 +16,15 @@ For use in the browser, use [browserify](https://github.com/substack/node-browse
 
 ## Usage
 
-To use the module,
-
 ``` javascript
 var leq = require( 'compute-leq' );
 ```
 
-#### leq( arr, x )
+#### leq( arr, x[, opts] )
 
 Computes an element-wise comparison (less than or equal to) for each input `array` element. `x` may either be an `array` of equal length or a single value (`number` or `string`).
 
-The function returns an `array` with length equal to that of the input `array`. Each output `array` element is either `0` or `1`. A value of `1` means that an element is less than or equal to a compared value and `0` means that an element is __not__ less than or equal to a compared value.
+The function returns an `array` with a length equal to that of the input `array`. Each output `array` element is either `0` or `1`. A value of `1` means that an element is less than or equal to a compared value and `0` means that an element is __not__ less than or equal to a compared value.
 
 ``` javascript
 var arr = [ 5, 3, 8, 3, 2 ],
@@ -41,6 +39,82 @@ out = leq( arr, [ 6, 3, 6, 7, 3 ] );
 // returns [ 1, 1, 0, 1, 1 ]
 ```
 
+The function accepts the following `options`:
+
+*  __copy__: `boolean` indicating whether to return a new `array`. Default: `true`.
+*  __accessor__: accessor `function` for accessing values in object `arrays`.
+
+To mutate the input `array` (e.g., when input values can be discarded or when optimizing memory usage), set the `copy` option to `false`.
+
+``` javascript
+var arr = [ 5, 3, 8, 3, 2 ];
+
+var out = leq( arr, 3, {
+	'copy': false
+});
+// returns [ 0, 1, 0, 1, 1 ]
+
+console.log( arr === out );
+// returns true
+```
+
+For object `arrays`, provide an accessor `function` for accessing `array` values.
+
+``` javascript
+var data = [
+	['beep', 5],
+	['boop', 3],
+	['bip', 8],
+	['bap', 3],
+	['baz', 2]
+];
+
+function getValue( d, i ) {
+	return d[ 1 ];
+}
+
+var out = leq( data, 3, {
+	'accessor': getValue
+});
+// returns [ 0, 1, 0, 1, 1 ]
+```
+
+When comparing values between two object `arrays`, provide an accessor `function` which accepts `3` arguments.
+
+``` javascript
+var data = [
+	['beep', 5],
+	['boop', 3],
+	['bip', 8],
+	['bap', 3],
+	['baz', 2]
+];
+
+var arr = [
+	{'x': 4},
+	{'x': 3},
+	{'x': 6},
+	{'x': 5},
+	{'x': 2}
+];
+
+function getValue( d, i, j ) {
+	if ( j === 0 ) {
+		return d[ 1 ];
+	}
+	return d.x;
+}
+
+var out = leq( data, arr, {
+	'accessor': getValue
+});
+// returns [ 0, 1, 0, 1, 1 ]
+```
+
+__Note__: `j` corresponds to the input `array` index, where `j=0` is the index for the first input `array` and `j=1` is the index for the second (comparison) input `array`.
+
+
+
 
 ## Examples
 
@@ -50,7 +124,6 @@ var leq = require( 'compute-leq' ),
 
 // Simulate some data...
 var data = new Array( 100 );
-
 for ( var i = 0; i < data.length; i++ ) {
 	data[ i ] = Math.round( Math.random()*100 );
 }
@@ -74,7 +147,7 @@ $ node ./examples/index.js
 
 ### Unit
 
-Unit tests use the [Mocha](http://visionmedia.github.io/mocha) test framework with [Chai](http://chaijs.com) assertions. To run the tests, execute the following command in the top-level application directory:
+Unit tests use the [Mocha](http://mochajs.org) test framework with [Chai](http://chaijs.com) assertions. To run the tests, execute the following command in the top-level application directory:
 
 ``` bash
 $ make test
@@ -98,15 +171,15 @@ $ make view-cov
 ```
 
 
+---
 ## License
 
 [MIT license](http://opensource.org/licenses/MIT). 
 
 
----
 ## Copyright
 
-Copyright &copy; 2014. Athan Reines.
+Copyright &copy; 2014-2015. Athan Reines.
 
 
 [npm-image]: http://img.shields.io/npm/v/compute-leq.svg
--- test/test.js
+++ test/test.js
@@ -1,3 +1,4 @@
+/* global require, describe, it */
 'use strict';
 
 // MODULES //
@@ -46,8 +47,10 @@ describe( 'compute-leq', function tests() {
 		}
 	});
 
-	it( 'should throw an error if a comparison value which is not an array of equal length, number, or string', function test() {
+	it( 'should throw an error if a comparison value which is not an array, number primitive, or string primitive', function test() {
 		var values = [
+			new Number( 5 ),
+			new String( '5' ),
 			null,
 			undefined,
 			NaN,
@@ -62,7 +65,80 @@ describe( 'compute-leq', function tests() {
 
 		function badValue( value ) {
 			return function() {
-				leq( [], value );
+				leq( [1,2,3], value );
+			};
+		}
+	});
+
+	it( 'should throw an error if provided an options argument which is not an object', function test() {
+		var values = [
+			'5',
+			5,
+			null,
+			undefined,
+			NaN,
+			true,
+			[],
+			function(){}
+		];
+
+		for ( var i = 0; i < values.length; i++ ) {
+			expect( badValue( values[i] ) ).to.throw( TypeError );
+		}
+
+		function badValue( value ) {
+			return function() {
+				leq( [1,2,3], 10, value );
+			};
+		}
+	});
+
+	it( 'should throw an error if provided a copy option which is not a boolean primitive', function test() {
+		var values = [
+			'5',
+			5,
+			null,
+			undefined,
+			NaN,
+			new Boolean( true ),
+			{},
+			[],
+			function(){}
+		];
+
+		for ( var i = 0; i < values.length; i++ ) {
+			expect( badValue( values[i] ) ).to.throw( TypeError );
+		}
+
+		function badValue( value ) {
+			return function() {
+				leq( [1,2,3], 10, {
+					'copy': value
+				});
+			};
+		}
+	});
+
+	it( 'should throw an error if provided an accessor option which is not a function', function test() {
+		var values = [
+			'5',
+			5,
+			null,
+			undefined,
+			NaN,
+			{},
+			[]
+		];
+
+		for ( var i = 0; i < values.length; i++ ) {
+			expect( badValue( values[i] ) ).to.throw( TypeError );
+		}
+
+		function badValue( value ) {
+			return function() {
+				leq( [1,2,3], 10, {
+					'accessor': value
+				});
 			};
 		}
 	});
@@ -74,12 +150,21 @@ describe( 'compute-leq', function tests() {
 		}
 	});
 
+	it( 'should not mutate an input array by default', function test() {
+		var data, actual;
+
+		data = [ 4, 5, 3, 6, 8 ];
+		actual = leq( data, 4 );
+
+		assert.ok( data !== actual );
+	});
+
 	it( 'should correctly compare values', function test() {
 		var data, expected, actual;
 
 		data = [ 4, 5, 3, 6, 8 ];
 
-		// Single numeric value:
+		// Single numeric comparator:
 		actual = leq( data, 4 );
 		expected = [ 1, 0, 1, 0, 0 ];
 
@@ -106,6 +191,103 @@ describe( 'compute-leq', function tests() {
 
 		actual = leq( data, [ 'aa', 'bb', 'aa', 'b' ] );
 		expected = [ 1, 1, 0, 1 ];
+
+		assert.deepEqual( actual, expected );
+	});
+
+	it( 'should mutate an input array if the `copy` option is `false`', function test() {
+		var data, expected, actual;
+
+		data = [ 4, 5, 3, 6, 8 ];
+
+		actual = leq( data, 4, {
+			'copy': false
+		});
+		expected = [ 1, 0, 1, 0, 0 ];
+
+		assert.ok( data === actual );
+		assert.deepEqual( actual, expected );
+	});
+
+	it( 'should correctly compare values using an accessor', function test() {
+		var data, expected, actual;
+
+		data = [
+			[1,4],
+			[2,5],
+			[3,3],
+			[4,6],
+			[5,8]
+		];
+
+		// Single numeric comparator:
+		actual = leq( data, 4, {
+			'accessor': function getValue( d ) {
+				return d[ 1 ];
+			}
+		});
+		expected = [ 1, 0, 1, 0, 0 ];
+
+		assert.deepEqual( actual, expected );
+
+		// Single string comparator:
+		data = [
+			{'x': 'a'},
+			{'x': 'aa'},
+			{'x': 'aaa'},
+			{'x': 'b'}
+		];
+
+		actual = leq( data, 'aa', {
+			'accessor': function getValue( d ) {
+				return d.x;
+			}
+		});
+		expected = [ 1, 1, 0, 0 ];
+
+		assert.deepEqual( actual, expected );
+	});
+
+	it( 'should correctly compare values when provided a comparison array and using an accessor', function test() {
+		var data, arr, expected, actual;
+
+		data = [
+			[1,4],
+			[2,5],
+			[3,3],
+			[4,6],
+			[5,8]
+		];
+
+		arr = [ 5, 5, 5, 5, 9 ];
+
+		actual = leq( data, arr, {
+			'accessor': function getValue( d ) {
+				return d[ 1 ];
+			}
+		});
+		expected = [ 1, 1, 1, 0, 1 ];
+
+		assert.deepEqual( actual, expected );
+
+		// Both arrays are accessed:
+		arr = [
+			{'x':5},
+			{'x':5},
+			{'x':5},
+			{'x':5},
+			{'x':9}
+		];
+
+		actual = leq( data, arr, {
+			'accessor': function getValue( d, i, j ) {
+				if ( j === 0 ) {
+					return d[ 1 ];
+				}
+				return d.x;
+			}
+		});
+		expected = [ 1, 1, 1, 0, 1 ];
 
 		assert.deepEqual( actual, expected );
 	});
